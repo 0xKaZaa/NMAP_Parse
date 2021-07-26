db065d8f6b8f30d364f7be97722aed0ce74079c0
KaZaa <87668793+0xKaZaa@users.noreply.github.com>
2021-07-26 09:55:35 -0400

    Update NMPS.sh

--- NMPS.sh
+++ NMPS.sh
@@ -7,7 +7,7 @@ while read service; do
         iplist=() #(re)sets the array to empty array                                                                                                                                          
         while read nmap_line; do #reads each line in nmap_refined and sees if it is an ip and then  takes ip from line if it is an ip                                                         
                 if [[ $nmap_line =~ $IP_REGEX ]]; then daip=$(echo $nmap_line | egrep '([0-9]{1,3}[.]){3}[0-9]{1,3}$'); fi #assigns ip to var to be added to array later                      
-                if [[ $nmap_line = $service ]]; then iplist+=("${daip}"); fi #checks if the line in the nmap_refined is a service and if so adds to the sevices counter 2(adds the ip to that$
+                if [[ $nmap_line = $service ]]; then iplist+=("${daip}"); fi #checks if the line in the nmap_refined is a service and if so adds to the sevices counter 2(adds the ip to that service)
         done<nmap_refined.txt                                                                                                                                                                 
         sorted_ips=($(echo "${iplist[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')) #gets rid of dup ips                                                                                                                                                                                                                                                     
         for i in "${sorted_ips[@]}"; do let counter++; done #for loop for ip count, post sort                                                                
